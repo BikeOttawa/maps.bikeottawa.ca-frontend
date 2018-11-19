2163fff0a761b441ea0e7aecd8e4f658edc2294c
Yaro <shkvorets@gmail.com>
2018-11-19 17:40:58 -0500

    Second (somewhat working) version with osm-request

--- winter/osm.js
+++ winter/osm.js
@@ -1,49 +1,29 @@
 const OsmRequest = require('osm-request');
-const osmAuth = require('osm-auth');
+
 
 const osm = new OsmRequest({
   endpoint: 'https://www.openstreetmap.org',
   oauthConsumerKey: 'ZgM987arjTsqx9SH8jknXREO12x5dcgNpTt66EjK',
   oauthSecret: 'ZV29qzTCGZ2vd5GMEVDyVRz6yK1C4vyrzc0z7FUy',
-  oauthUserToken: 'sghksxC7dDAra207oXGY4WIJX6ddXZrvixnrlzjL',
-//  oauthUserTokenSecret: 'yvt9PMGgv8dGDPKw2s6ReHjBbBBkfUHBgEOmPus9',
+  oauthUserToken: 'uay6rh6McCIIZ2HCSyO1tf2xpYdljPUT0ufWIPwv',         //BikeOttawaMaps
+  oauthUserTokenSecret: 'FklCZos2wjGMaqtH3NKc0gWXxwsZSFBdmLkwSf6l',   //BikeOttawaMaps
 });
 
 
-global.sendOsmData = async function () {
-  let element = await osm.fetchElement('way/39169225');
-  element = osm.setProperty(element, 'key', 'value');
-  element = osm.setProperties(element, {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-  });
-  element = osm.removeProperty(element, 'key2');
-  element = osm.setTimestampToNow(element);
+let changesetId = 0;
 
-  const changesetId = await osm.createChangeset('Created by me', 'Test comment');
-  //const isChangesetStillOpen = await osm.isChangesetStillOpen(changesetId);
-  const newElementVersion = await osm.sendElement(element, changesetId);
-  //element = osm.setVersion(element, newElementVersion);
+global.submitOsmChangeset = async function (osmElement, tags) {
+    let element = await osm.fetchElement(osmElement);
+    for (const key of Object.keys(tags)) {
+      element = osm.setProperty(element, key, tags[key]);
+    }
+    //element = osm.removeProperty(element, 'key2');
+    //element = osm.setTimestampToNow(element);
+
+    const changesetId = await osm.createChangeset('BikeOttawaMaps', 'Changed pathway details - https://maps.bikeottawa.ca');
+    //const changesetId = await osm.isChangesetStillOpen(changesetId);
+    const newElementVersion = await osm.sendElement(element, changesetId);
+
+    //element = osm.setVersion(element, newElementVersion);
+
 }
-
-global.sendOsmData1 = async function () {
-  let element = await osm.fetchElement('way/39169225');
-  element = osm.setProperty(element, 'key', 'value');
-  element = osm.setProperties(element, {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-  });
-  element = osm.removeProperty(element, 'key2');
-  element = osm.setTimestampToNow(element);
-
-
-  const changesetId = await osm.createChangeset('Created by me', 'Test comment');
-  const isChangesetStillOpen = await osm.isChangesetStillOpen(changesetId);
-  const newElementVersion = await osm.sendElement(element, changesetId);
-  //element = osm.setVersion(element, newElementVersion);
-}
-
-
-//sendOsmData1();
